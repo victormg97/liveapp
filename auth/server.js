9a05cd430b16c155a1b15a4cda5159e97e4080ad
victormg97 <vmaldonadog97@gmail.com>
2023-12-01 09:12:45 -0300

    Última versión

--- auth/server.js
+++ auth/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Importa el paquete cors
+
 const app = express();
+app.use(cors()); // Usa el middleware cors
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let isStreaming = false;
 
 app.use(express.urlencoded());
 
@@ -10,11 +19,31 @@ app.post("/auth", function (req, res) {
     /* Se puede hacer una base de datos para guardar las keys */
     if (streamkey === "supersecret"){
         res.status(200).send();
+        // Respuesta HTTP
+        res.status(200).json({ message: 'Stream started successfully' });
+        isStreaming = true;
+
+        // Emite evento al cliente
+        io.emit('streaming', { status: 'start' });
         return;
     }
 
     /* Rechazar el stream  */
     res.status(403).send();
+});
+
+app.post("/done", function (req, res) {
+    isStreaming = false;
+    res.status(200).send();
+    res.status(200).json({ message: 'Stream stopped successfully' });
+
+    // Emite evento al cliente
+    io.emit('streaming', { status: 'stop' });
+});
+
+app.get('/status', (req, res) => {
+    // Respuesta HTTP con el estado actual
+    res.status(200).json({ streaming: isStreaming });
 });
 
 app.listen(8000, function(){
